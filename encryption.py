d35a4c5ae67a612c23e2532dcd774a87f99e0282
castle9612 <castle9612@gmail.com>
2023-06-16 02:52:18 +0900

    final
    final

--- encryption.py
+++ encryption.py
@@ -1,282 +1,285 @@
-from Crypto.Cipher import AES, DES, DES3
-from Crypto.Util.Padding import pad, unpad
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from Crypto.Cipher import DES, AES, DES3
+from Crypto.Util.Padding import pad, unpad
 import os
 import datetime
-import threading
-
-
-class FileSelector:
-    def __init__(self, root):
-        self.root = root
-        self.filename = []
-
-        self.select_btn = tk.Button(
-            self.root,
-            text="파일 선택",
-            command=self.select_files
-        )
-        self.select_btn.pack(pady=10)
-
-    def select_files(self):
-        self.filename = filedialog.askopenfilenames()
-
-
-class CryptoProcessor:
-    def __init__(self, root):
-        self.root = root
-
-        self.mode_var = tk.StringVar(value="기법선택")
-        self.mode_var.trace("w", self.mode_changed)
-        self.mode_var1 = tk.StringVar(value="기법선택")
-        self.mode_var1.trace("w", self.mode_changed)
-
-        self.mode_label = tk.Label(self.root, text="암호화/복호화 기법 선택")
-        self.mode_label.pack()
-
-        self.mode_menu = tk.OptionMenu(self.root, self.mode_var, "기법선택", "DES암호화", "AES암호화", "3DES암호화")
-        self.mode_menu.pack()
-
-        self.mode_menu1 = tk.OptionMenu(self.root, self.mode_var1, "기법선택", "DES복호화", "AES복호화", "3DES복호화")
-        self.mode_menu1.pack()
-
-    def mode_changed(self, *args):
-        mode = self.mode_var.get()
-        mode1 = self.mode_var1.get()
-        if mode != "기법선택" and mode1 != "기법선택":
-            self.mode_var.set("기법선택")
-            self.mode_var1.set("기법선택")
-            messagebox.showwarning("오류!", "암호화와 복호화 중 한 가지를 선택해주세요.")
-
-
-class PasswordInput:
-    def __init__(self, root):
-        self.root = root
-
-        self.password_label = tk.Label(self.root, text="비밀번호 입력")
-        self.password_label.pack()
-
-        self.password_entry = tk.Entry(self.root, show="*")
-        self.password_entry.pack(pady=10)
-    def get_password(self):
-            password = self.password_entry.get()
-            if not password:
-                # 기본 암호 설정 (예: "default_password")
-                password = "abcdef0123456789"
-            return password
-
-
-class ScheduleTask:
-    def __init__(self, root, callback):
-        self.root = root
-        self.callback = callback
-
-        self.schedule_label = tk.Label(self.root, text="일정 시간 후 실행")
-        self.schedule_label.pack()
-
-        self.time_entry = tk.Entry(self.root)
-        self.time_entry.pack()
-
-        self.schedule_btn = tk.Button(
-            self.root,
-            text="일정 시간 후 실행",
-            command=self.schedule_execution
-        )
-        self.schedule_btn.pack(pady=10)
-
-    def schedule_execution(self):
-        time_str = self.time_entry.get()
-        if not time_str:
-            self.callback()
-            return
-
-        try:
-            scheduled_time = datetime.datetime.strptime(time_str, "%H:%M:%S").time()
-        except ValueError:
-            messagebox.showwarning("오류!", "올바른 시간 형식을 입력해주세요. (HH:MM:SS)")
-            return
-
-        current_time = datetime.datetime.now().time()
-        time_diff = datetime.datetime.combine(datetime.date.today(), scheduled_time) - datetime.datetime.combine(
-            datetime.date.today(), current_time
-        )
-        time_diff_seconds = time_diff.total_seconds()
-
-        if time_diff_seconds <= 0:
-            messagebox.showwarning("오류!", "과거의 시간을 입력했습니다. 미래의 시간을 입력해주세요.")
-            return
-
-        self.root.after(int(time_diff_seconds * 1000), self.callback)
 
 
 class CryptoApp:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("파일 암호화/복호화")
-
+        self.root.geometry("600x400")
+        self.root.title("파일 암호화/복호화 프로그램")
         self.file_selector = FileSelector(self.root)
+        self.file_selector.pack()
+
         self.crypto_processor = CryptoProcessor(self.root)
+        self.crypto_processor.pack()
+
         self.password_input = PasswordInput(self.root)
-        self.schedule_task = ScheduleTask(self.root, self.execute_crypto)
+        self.password_input.pack()
 
-        self.execute_btn = tk.Button(
-            self.root,
-            text="암호화/복호화 실행",
-            command=self.execute_crypto
-        )
-        self.execute_btn.pack(pady=20)
+        self.schedule_time = ScheduleTime(self.root)
+        self.schedule_time.pack()
 
-        self.delete_checkbox = tk.Checkbutton(
-            self.root,
-            text="원본 파일 삭제",
-            onvalue=1,
-            offvalue=0
-        )
-        self.delete_checkbox.pack()
+        self.execute_button = tk.Button(self.root, text="실행", command=self.execute_crypto)
+        self.execute_button.pack()
 
         self.root.mainloop()
 
+    def encrypt_file_DES(self, filename, password):
+        key = password.encode("utf-8").ljust(8, b"\x00")[:8]
+        cipher = DES.new(key, DES.MODE_ECB)
+
+        with open(filename, "rb") as file:
+            plaintext = file.read()
+
+        encrypted_ciphertext = cipher.encrypt(pad(plaintext, DES.block_size))
+
+        encrypted_filename = filename + ".cas"
+        with open(encrypted_filename, "wb") as file:
+            file.write(encrypted_ciphertext)
+
+        return encrypted_filename
+    
+    def encrypt_file_AES(self, filename, password):
+        key = password.encode("utf-8").ljust(16, b"\x00")[:16]
+        cipher = AES.new(key, AES.MODE_ECB)
+
+        with open(filename, "rb") as file:
+            plaintext = file.read()
+
+        encrypted_ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
+
+        encrypted_filename = filename + ".cas"
+        with open(encrypted_filename, "wb") as file:
+            file.write(encrypted_ciphertext)
+
+        return encrypted_filename
+
+    def encrypt_file_3DES(self, filename, password):
+        key = password.encode("utf-8").ljust(24, b"\x00")[:24]
+        cipher = DES3.new(key, DES3.MODE_ECB)
+
+        with open(filename, "rb") as file:
+            plaintext = file.read()
+
+        encrypted_ciphertext = cipher.encrypt(pad(plaintext, DES3.block_size))
+
+        encrypted_filename = filename + ".cas"
+        with open(encrypted_filename, "wb") as file:
+            file.write(encrypted_ciphertext)
+
+        return encrypted_filename
+
+    def decrypt_file_DES(self, filename, password):
+        key = password.encode("utf-8").ljust(8, b"\x00")[:8]
+        cipher = DES.new(key, DES.MODE_ECB)
+
+        with open(filename, "rb") as file:
+            ciphertext = file.read()
+
+        decrypted_plaintext = unpad(cipher.decrypt(ciphertext), DES.block_size)
+
+        decrypted_filename = filename[:-4]
+        with open(decrypted_filename, "wb") as file:
+            file.write(decrypted_plaintext)
+
+        return decrypted_filename
+
+    def decrypt_file_AES(self, filename, password):
+        key = password.encode("utf-8").ljust(16, b"\x00")[:16]
+        cipher = AES.new(key, AES.MODE_ECB)
+
+        with open(filename, "rb") as file:
+            ciphertext = file.read()
+
+        decrypted_plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+
+        decrypted_filename = filename[:-4]
+        with open(decrypted_filename, "wb") as file:
+            file.write(decrypted_plaintext)
+
+        return decrypted_filename
+
+    def decrypt_file_3DES(self, filename, password):
+        key = password.encode("utf-8").ljust(24, b"\x00")[:24]
+        cipher = DES3.new(key, DES3.MODE_ECB)
+
+        with open(filename, "rb") as file:
+            ciphertext = file.read()
+
+        decrypted_plaintext = unpad(cipher.decrypt(ciphertext), DES3.block_size)
+
+        decrypted_filename = filename[:-4]
+        with open(decrypted_filename, "wb") as file:
+            file.write(decrypted_plaintext)
+
+        return decrypted_filename
+
+    def get_original_extension(self, filename):
+        _, ext = os.path.splitext(filename)
+        return ext
+
     def execute_crypto(self):
-        mode = self.crypto_processor.mode_var.get()
-        password = self.password_input.get_password()
+        selected_files = self.file_selector.get_selected_files()
+        selected_mode = self.crypto_processor.get_selected_mode()
+        scheduled_time = self.schedule_time.get_scheduled_time()
 
-        if not password:
-            # Use a default password if none is entered
-            password = "default_password"
-
-        try:
-            password_bytes = password.encode()
-            if len(password_bytes) != 8 and mode.startswith("DES"):
-                messagebox.showwarning("오류!", "비밀번호는 8바이트여야 합니다.")
-                return
-
-            if len(password_bytes) != 16 and mode.startswith("AES"):
-                messagebox.showwarning("오류!", "비밀번호는 16바이트여야 합니다.")
-                return
-        except UnicodeEncodeError:
-            messagebox.showwarning("오류!", "올바른 문자열을 입력해주세요.")
+        if not selected_files:
+            messagebox.showerror("Error", "파일을 선택해주세요.")
             return
 
-        if mode == "DES암호화":
-            self.encrypt_file_DES(DES, password_bytes)
-        elif mode == "AES암호화":
-            self.encrypt_file_AES(AES, password_bytes)
-        elif mode == "3DES암호화":
-            self.encrypt_file_DES(DES3, password_bytes)
-        elif mode == "DES복호화":
-            self.decrypt_file_DES(DES, password_bytes)
-        elif mode == "AES복호화":
-            self.decrypt_file_AES(AES, password_bytes)
-        elif mode == "3DES복호화":
-            self.decrypt_file_DES(DES3, password_bytes)
+        if selected_mode == "기법선택":
+            messagebox.showerror("Error", "암호화 또는 복호화 기법을 선택해주세요.")
+            return
+        
 
-    def encrypt_file_DES(self, cipher_algo, password_bytes):
-        for filename in self.file_selector.filename:
-            with open(filename, "rb") as f:
-                plaintext = f.read()
+        current_time = datetime.datetime.now()
 
-            key = password_bytes
-            iv = b"thisivv!"
+        if scheduled_time and current_time < scheduled_time:
+            time_diff = scheduled_time - current_time
+            self.root.after(int(time_diff.total_seconds() * 1000), self.execute_crypto_now)
+        else:
+            self.execute_crypto_now()
 
-            cipher = cipher_algo.new(key, cipher_algo.MODE_CBC, iv)
-            padded_plaintext = pad(plaintext, cipher_algo.block_size)
-            ciphertext = cipher.encrypt(padded_plaintext)
+    def execute_crypto_now(self):
+        selected_files = self.file_selector.get_selected_files()
+        selected_mode = self.crypto_processor.get_selected_mode()
+        password, delete_original = self.password_input.get_password(selected_mode)
 
-            with open(filename + ".DESenc", "wb") as f:
-                f.write(ciphertext)
+        if not selected_files:
+            messagebox.showerror("Error", "파일을 선택해주세요.")
+            return
 
-            if self.delete_checkbox.get() == 1:
-                # 원본 파일 삭제
-                os.remove(filename)
+        if selected_mode == "기법선택":
+            messagebox.showerror("Error", "암호화 또는 복호화 기법을 선택해주세요.")
+            return
 
-            print(f"암호화된 파일: {filename + '.DESenc'}")
-            print(f"암호화된 데이터 예시: {ciphertext[:16]}...")
+        for file in selected_files:
+            try:
+                if selected_mode == "DES암호화":
+                    encrypted_filename = self.encrypt_file_DES(file, password)
+                    messagebox.showinfo("암호화 완료", f"{file}이(가) 암호화되었습니다. 암호화된 파일: {encrypted_filename}")
 
-        messagebox.showinfo('알림!!', 'DES로 암호화가 완료되었습니다!')
+                elif selected_mode == "AES암호화":
+                    encrypted_filename = self.encrypt_file_AES(file, password)
+                    messagebox.showinfo("암호화 완료", f"{file}이(가) 암호화되었습니다. 암호화된 파일: {encrypted_filename}")
 
-    def encrypt_file_AES(self, cipher_algo, password_bytes):
-        for filename in self.file_selector.filename:
-            with open(filename, "rb") as f:
-                plaintext = f.read()
+                elif selected_mode == "3DES암호화":
+                    encrypted_filename = self.encrypt_file_3DES(file, password)
+                    messagebox.showinfo("암호화 완료", f"{file}이(가) 암호화되었습니다. 암호화된 파일: {encrypted_filename}")
 
-            key = password_bytes
-            iv = b"thisisiv!123456"
+                elif selected_mode == "DES복호화":
+                    decrypted_filename = self.decrypt_file_DES(file, password)
+                    messagebox.showinfo("복호화 완료", f"{file}이(가) 복호화되었습니다. 복호화된 파일: {decrypted_filename}")
 
-            cipher = cipher_algo.new(key, cipher_algo.MODE_CBC, iv)
-            padded_plaintext = pad(plaintext, cipher_algo.block_size)
-            ciphertext = cipher.encrypt(padded_plaintext)
+                elif selected_mode == "AES복호화":
+                    decrypted_filename = self.decrypt_file_AES(file, password)
+                    messagebox.showinfo("복호화 완료", f"{file}이(가) 복호화되었습니다. 복호화된 파일: {decrypted_filename}")
 
-            with open(filename + ".AESenc", "wb") as f:
-                f.write(ciphertext)
+                elif selected_mode == "3DES복호화":
+                    decrypted_filename = self.decrypt_file_3DES(file, password)
+                    messagebox.showinfo("복호화 완료", f"{file}이(가) 복호화되었습니다. 복호화된 파일: {decrypted_filename}")
 
-            if self.delete_checkbox["variable"].get() == 1:
-                # 원본 파일 삭제
-                os.remove(filename)
+                if delete_original:
+                    os.remove(file)
 
-            print(f"암호화된 파일: {filename + '.AESenc'}")
-            print(f"암호화된 데이터 예시: {ciphertext[:16]}...")
+            except Exception as e:
+                messagebox.showerror("Error", f"처리 중 오류가 발생했습니다: {str(e)}")
 
-        messagebox.showinfo('알림!!', 'AES로 암호화가 완료되었습니다!')
 
-    def decrypt_file_DES(self, cipher_algo, password_bytes):
-        for filename in self.file_selector.filename:
-            if not filename.endswith(".DESenc"):
-                continue
+class FileSelector(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.selected_files = []
 
-            with open(filename, "rb") as f:
-                ciphertext = f.read()
+        self.select_button = tk.Button(self, text="파일 선택", command=self.select_files)
+        self.select_button.pack()
 
-            key = password_bytes
-            iv = b"thisivv!"
+        self.selected_files_label = tk.Label(self, text="선택한 파일: ")
+        self.selected_files_label.pack()
 
-            cipher = cipher_algo.new(key, cipher_algo.MODE_CBC, iv)
-            padded_plaintext = cipher.decrypt(ciphertext)
-            plaintext = unpad(padded_plaintext, cipher_algo.block_size)
+    def select_files(self):
+        self.selected_files = filedialog.askopenfilenames()
+        self.update_selected_files_label()
 
-            decrypted_filename = filename[:-7]  # Remove ".DESenc" extension
+    def update_selected_files_label(self):
+        file_names = ", ".join(self.selected_files) if self.selected_files else "없음"
+        self.selected_files_label.configure(text="선택한 파일: " + file_names)
 
-            with open(decrypted_filename, "wb") as f:
-                f.write(plaintext)
+    def get_selected_files(self):
+        return self.selected_files
 
-            if self.delete_checkbox["variable"].get() == 1:
-                # 원본 파일 삭제
-                os.remove(filename)
 
-            print(f"복호화된 파일: {decrypted_filename}")
-            print(f"복호화된 데이터 예시: {plaintext[:16]}...")
+class CryptoProcessor(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.mode_var1 = tk.StringVar()
+        self.mode_var1.set("기법선택")
 
-        messagebox.showinfo('알림!!', 'DES로 복호화가 완료되었습니다!')
+        self.mode_label = tk.Label(self, text="암호화/복호화 기법 선택")
+        self.mode_label.pack()
 
-    def decrypt_file_AES(self, cipher_algo, password_bytes):
-        for filename in self.file_selector.filename:
-            if not filename.endswith(".AESenc"):
-                continue
+        self.mode_menu = tk.OptionMenu(self, self.mode_var1, "기법선택", "DES암호화", "AES암호화", "3DES암호화", "DES복호화", "AES복호화", "3DES복호화")
+        self.mode_menu.pack()
 
-            with open(filename, "rb") as f:
-                ciphertext = f.read()
+    def get_selected_mode(self):
+        return self.mode_var1.get()
 
-            key = password_bytes
-            iv = b"thisisiv!123456"
 
-            cipher = cipher_algo.new(key, cipher_algo.MODE_CBC, iv)
-            padded_plaintext = cipher.decrypt(ciphertext)
-            plaintext = unpad(padded_plaintext, cipher_algo.block_size)
+class PasswordInput(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.password_var = tk.StringVar()
+        self.password_var.set("")
 
-            decrypted_filename = filename[:-7]  # Remove ".AESenc" extension
+        self.password_label = tk.Label(self, text="암호 입력")
+        self.password_label.pack()
 
-            with open(decrypted_filename, "wb") as f:
-                f.write(plaintext)
+        self.password_entry = tk.Entry(self, textvariable=self.password_var, show="*")
+        self.password_entry.pack()
 
-            if self.delete_checkbox["variable"].get() == 1:
-                # 원본 파일 삭제
-                os.remove(filename)
+        self.delete_original_var = tk.BooleanVar()
+        self.delete_original_var.set(False)
 
-            print(f"복호화된 파일: {decrypted_filename}")
-            print(f"복호화된 데이터 예시: {plaintext[:16]}...")
+        self.delete_original_checkbutton = tk.Checkbutton(
+            self, text="원본 파일 삭제", variable=self.delete_original_var
+        )
+        self.delete_original_checkbutton.pack()
 
-        messagebox.showinfo('알림!!', 'AES로 복호화가 완료되었습니다!')
+    def get_password(self, selected_mode):
+        password = self.password_var.get()
+        delete_original = self.delete_original_var.get()
+
+        if selected_mode == "기법선택":
+            password = ""
+            delete_original = False
+
+        return password, delete_original
+
+
+class ScheduleTime(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.schedule_label = tk.Label(self, text="예약 시간 (YYYY-MM-DD HH:MM:SS):")
+        self.schedule_label.pack()
+
+        self.schedule_entry = tk.Entry(self)
+        self.schedule_entry.pack()
+
+    def get_scheduled_time(self):
+        scheduled_time_str = self.schedule_entry.get()
+        if scheduled_time_str:
+            try:
+                scheduled_time = datetime.datetime.strptime(scheduled_time_str, "%Y-%m-%d %H:%M:%S")
+                return scheduled_time
+            except ValueError:
+                messagebox.showerror("Error", "올바른 날짜 및 시간 형식을 입력해주세요.")
+        return None
 
 
 if __name__ == "__main__":
-    CryptoApp()
+    app = CryptoApp()
